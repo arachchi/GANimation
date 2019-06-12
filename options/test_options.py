d982b4c2e151b5fbf5464f75a19cccf56e6821e2
qishen-ha <qishen.ha@linecorp.com>
2019-06-12 15:29:53 +0900

    change some option

--- options/test_options.py
+++ options/test_options.py
@@ -4,6 +4,6 @@ from .base_options import BaseOptions
 class TestOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
-        self._parser.add_argument('--input_path', type=str, help='path to image')
-        self._parser.add_argument('--output_dir', type=str, default='./output', help='output path')
+        self._parser.add_argument('-in', '--input_path', type=str, help='path to image')
+        self._parser.add_argument('-out', '--output_dir', type=str, default='./output', help='output path')
         self.is_train = False
--- options/train_options.py
+++ options/train_options.py
@@ -4,14 +4,14 @@ from .base_options import BaseOptions
 class TrainOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
-        self._parser.add_argument('--n_threads_train', default=0, type=int, help='# threads for loading data')
+        self._parser.add_argument('--n_threads_train', default=8, type=int, help='# threads for loading data')
         self._parser.add_argument('--num_iters_validate', default=1, type=int, help='# batches to use when validating')
         self._parser.add_argument('--print_freq_s', type=int, default=60, help='frequency of showing training results on console')
         self._parser.add_argument('--display_freq_s', type=int, default=300, help='frequency [s] of showing training results on screen')
         self._parser.add_argument('--save_latest_freq_s', type=int, default=3600, help='frequency of saving the latest results')
 
-        self._parser.add_argument('--nepochs_no_decay', type=int, default=20, help='# of epochs at starting learning rate')
-        self._parser.add_argument('--nepochs_decay', type=int, default=10, help='# of epochs to linearly decay learning rate to zero')
+        self._parser.add_argument('--nepochs_no_decay', type=int, default=40, help='# of epochs at starting learning rate')
+        self._parser.add_argument('--nepochs_decay', type=int, default=40, help='# of epochs to linearly decay learning rate to zero')
 
         self._parser.add_argument('--train_G_every_n_iterations', type=int, default=5, help='train G every n interations')
         self._parser.add_argument('--poses_g_sigma', type=float, default=0.06, help='initial learning rate for adam')
